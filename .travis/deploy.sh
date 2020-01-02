6f11b85016963081a0060f4da97581a77f5b85a6
git <jack.rickards@hotmail.co.uk>
2020-01-02 23:08:44 +0000

    final config done?

---
+++ .travis/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git config --global push.default simple # we only want to push one branch — master
+# specify the repo on the live server as a remote repo, and name it 'production'
+# <user> here is the separate user you created for deploying
+git remote add production ssh://git@157.245.37.38/home/git/prod_portfolio
+git push production master # push our updates
